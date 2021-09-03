8740a0aaeadff8f41104c4c8b702264d28e41fcf
Nataliia-Korneiko <53409659+Nataliia-Korneiko@users.noreply.github.com>
2021-09-03 23:19:12 +0300

    added 01-node-tutorial

---
+++ 01-node-tutorial/11-fs-async.js
@@ -0,0 +1,36 @@
+const { readFile, writeFile } = require('fs');
+console.log('Start!');
+
+readFile('./content/first.txt', 'utf8', (error, result) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  console.log('result:', result);
+
+  const first = result;
+  readFile('./content/second.txt', 'utf8', (error, result) => {
+    if (error) {
+      console.log(error);
+      return;
+    }
+
+    const second = result;
+    writeFile(
+      './content/result-async.txt',
+      `Here is the result: ${first}, ${second}`,
+      (error, result) => {
+        if (error) {
+          console.log(error);
+          return;
+        }
+
+        console.log('result:', result);
+        console.log('Done with this task!');
+      }
+    );
+  });
+});
+
+console.log('Starting next task!');
