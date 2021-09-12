6de19a39c1c5d66b0bb8be06a9b62a96149f85be
Nataliia-Korneiko <53409659+Nataliia-Korneiko@users.noreply.github.com>
2021-09-12 23:20:12 +0300

    finished 02-express-tutorial

---
+++ 02-express-tutorial/controller/auth.js
@@ -0,0 +1,13 @@
+const auth = (req, res) => {
+  const { name } = req.body;
+
+  if (name) {
+    return res.status(200).send(`Welcome ${name}`);
+  }
+
+  res.status(401).send('Please Provide Credentials');
+};
+
+module.exports = {
+  auth,
+};
