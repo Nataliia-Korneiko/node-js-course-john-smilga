3d49e3b1a68e5cd4cb0ab2534b710065693dba8a
Nataliia-Korneiko <53409659+Nataliia-Korneiko@users.noreply.github.com>
2021-09-20 23:57:00 +0300

    added 05-jwt-basics

---
+++ 05-jwt-basics/app.js
@@ -0,0 +1,25 @@
+require('express-async-errors');
+require('dotenv').config();
+const express = require('express');
+const connectDB = require('./db/connect');
+const notFound = require('./middleware/not-found');
+const errorHandler = require('./middleware/error-handler');
+
+const app = express();
+const { DB_CONNECTION, PORT = 5000 } = process.env;
+
+app.use(express.static('./public'));
+app.use(express.json());
+app.use(notFound);
+app.use(errorHandler);
+
+const start = async () => {
+  try {
+    await connectDB(DB_CONNECTION);
+    app.listen(PORT, console.log(`Server is listening on port: ${PORT}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
