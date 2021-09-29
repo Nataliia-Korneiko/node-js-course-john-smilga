7a952855f046c5ba78376786b79d9394dcf6fcbc
Nataliia-Korneiko <53409659+Nataliia-Korneiko@users.noreply.github.com>
2021-09-30 01:05:45 +0300

    finished 06-jobs-api

--- 06-jobs-api/app.js
+++ 06-jobs-api/app.js
@@ -1,6 +1,13 @@
 require('express-async-errors');
 require('dotenv').config();
 const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
 const authRoutes = require('./routes/auth');
 const jobsRoutes = require('./routes/jobs');
 const connectDB = require('./db/connect');
@@ -11,8 +18,23 @@ const authenticateUser = require('./middleware/auth');
 const app = express();
 const { DB_CONNECTION, PORT = 5000 } = process.env;
 
+app.set('trust proxy', 1); // enable if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+);
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
+app.get('/', (req, res) => {
+  res.send('<h1>Jobs API</h1><a href="/api/v1/docs">Documentation</a>');
+});
+
+app.use('/api/v1/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/jobs', authenticateUser, jobsRoutes); // authenticateUser - protected routes
 
